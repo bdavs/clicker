9d8d0017ef4bf0a5a12410b35bec90ea82ca1bef
bdavs <rdavis4677@gmail.com>
2020-04-25 23:38:25 -0400

    Updates

---
+++ precache-manifest.1cc2b30cd94df2a779410df7760b530a.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08c58f54a8bfd4d2d17f26c39a7e9fdd",
+    "url": "/clicker/index.html"
+  },
+  {
+    "revision": "58098e43c53a1cc64d30",
+    "url": "/clicker/static/css/2.7de149a3.chunk.css"
+  },
+  {
+    "revision": "ab8d4a34041bac6f67e9",
+    "url": "/clicker/static/css/main.6650010e.chunk.css"
+  },
+  {
+    "revision": "58098e43c53a1cc64d30",
+    "url": "/clicker/static/js/2.ff59deb1.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/clicker/static/js/2.ff59deb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab8d4a34041bac6f67e9",
+    "url": "/clicker/static/js/main.796124a9.chunk.js"
+  },
+  {
+    "revision": "325d2936217eda3f1768",
+    "url": "/clicker/static/js/runtime-main.874753b8.js"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "/clicker/static/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "/clicker/static/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "/clicker/static/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "/clicker/static/media/notification.c0d3c94c.eot"
+  }
+]);
--- src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux'
-import notification from './notification'
-import counter from './counter'
-
-export default combineReducers({
-  notification,
-  counter
-})
