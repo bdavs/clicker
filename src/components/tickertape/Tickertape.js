1a76adf70daf50ea4b9e6f3637836e6ced1bfa78
bdavs <rdavis4677@gmail.com>
2020-04-26 22:28:49 -0400

    make tickertape more dynamic timing and text

--- src/components/tickertape/Tickertape.js
+++ src/components/tickertape/Tickertape.js
@@ -1,31 +1,25 @@
 import React, { Component } from "react";
-import {connect} from "react-redux";
-// import BigNumber from "bignumber"
-// const BigNumber = require('bignumber.js');
-import "./tickertape.css"
+import { connect } from "react-redux";
 
+import "./tickertape.css";
 
+function tickerText() {
+  const text = "this is a shitload of text like so much juck copious amountsticker this is a shitload of text like so much juck copious amountstickerthis is a shitload of text like so much juck copious amountsticker"
+  // const text = "hi"
+  const tickerTime = Math.max(text.length / 30, 20)
+  const tickerStyle = {animationDuration: tickerTime+"s"}
+  return (
+    <p style={tickerStyle}>
+      {text}
+    </p>
+  );
+}
 class Tickertape extends Component {
   render() {
-    const {totalClicks} = this.props;
-    // const bntc = BigNumber(totalClicks);
-    // let myfmt = fmt;
-    // let ending = '';
-    // let result = 0;
-
-    
-    // const displayedClicks = result + ' ' + ending
-
-    // bntc.toFormat(0,0,myfmt)
-    
-    return (
-    <div className="marquee">
-      
-      <p>
-      this is a shitload of text like so much juck copious amountsticker this is a shitload of text like so much juck copious amountstickerthis is a shitload of text like so much juck copious amountsticker
-      </p>
-    </div>
-    );
+    // const {totalClicks} = this.props;
+    return <div className="marquee">
+      {tickerText()}
+    </div>;
   }
 }
 
